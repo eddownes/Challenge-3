81ca0255be476349d9ae1c1e1f9cdaa4509e28a6
Ed Downes <eddownes@me.com>
2019-11-19 01:53:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function copyAndSortNumbers(numbers) {
+  
+}
+
+/* Do not modify code below this line */
+
+const original = [1, 7, 3, 5];
+const sortedCopy = copyAndSortNumbers(original);
+
+console.log(original, '<-- should be [1, 7, 3, 5]');
+console.log(sortedCopy, '<-- should be [1, 3, 5, 7]');
